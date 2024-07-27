d27bac8c19c97f50b50395b0a3ca57282c5c2b24
SvitLanaTy <stymchenko@gmail.com>
2024-07-27 17:41:17 +0200

    add section

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,28 @@ import TransactionHistory from "./components/TransactionHistory/TransactionHisto
 import userData from "./data/userData.json";
 import friends from "./data/friends.json";
 import transactions from "./data/transactions.json";
+import Section from "./components/Section/Section";
 
 const App = () => {
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Section>
+        <Profile
+          name={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          image={userData.avatar}
+          stats={userData.stats}
+        />
+      </Section>
+
+      <Section>
+        <FriendList friends={friends} />
+      </Section>
+
+      <Section>
+        <TransactionHistory items={transactions} />
+      </Section>
     </>
   );
 };
